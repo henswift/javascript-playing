19715a4c0b46fd82f4564b3fb45c4e3aeb7be519
henswift <henweaver@icloud.com>
2023-10-16 19:51:00 -0400

    Added the students js.

---
+++ students.js
@@ -0,0 +1,9 @@
+let student = {
+    name: 'Hen',
+    grade: 60,
+    badStudent: true,
+    hobbies: [
+        {type: 'rcAircaft', dangerLvL: 5},
+        {type: 'Computers', dangerLvL: 2},
+    ]
+}
